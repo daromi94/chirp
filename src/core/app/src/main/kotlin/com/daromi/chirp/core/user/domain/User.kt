dc3b5a96cda047b62b3c6db2acc3c94658d9bc2f
daromi94 <davromero07@gmail.com>
2024-08-04 15:03:22 -0500

    feat: :sparkles: add user aggregate and related value objects

--- src/core/app/src/main/kotlin/com/daromi/chirp/core/user/domain/User.kt
+++ src/core/app/src/main/kotlin/com/daromi/chirp/core/user/domain/User.kt
@@ -1,51 +1,127 @@
 package com.daromi.chirp.core.user.domain
 
+import arrow.core.Either
+import arrow.core.raise.either
+import arrow.core.raise.ensure
+import com.daromi.chirp.core.shared.Error
 import java.time.LocalDateTime
+
+class User private constructor(
+    private val _id: UserId,
+    private var _name: UserName,
+    private var _handle: UserHandle,
+    private val _createdAt: UserCreatedAt,
+    private var _updatedAt: UserUpdatedAt,
+) {
+    companion object {
+        fun create(
+            id: String,
+            name: String,
+            handle: String,
+            createdAt: LocalDateTime,
+        ): User =
+            User(
+                UserId(id),
+                UserName(name),
+                UserHandle(handle),
+                UserCreatedAt(createdAt),
+                UserUpdatedAt(createdAt),
+            )
+    }
+
+    val id: String get() = _id.value
+
+    val name: String get() = _name.value
+
+    val handle: String get() = _handle.value
+
+    val createdAt: LocalDateTime get() = _createdAt.value
+
+    val updatedAt: LocalDateTime get() = _updatedAt.value
+
+    fun updateName(
+        name: String,
+        updatedAt: LocalDateTime,
+    ) {
+        this._name = UserName(name)
+        this._updatedAt = UserUpdatedAt(updatedAt)
+    }
+
+    fun updateHandle(
+        handle: String,
+        updatedAt: LocalDateTime,
+    ) {
+        this._handle = UserHandle(handle)
+        this._updatedAt = UserUpdatedAt(updatedAt)
+    }
+
+    override fun toString(): String = "User(id=$id, name=$name, handle=$handle, createdAt=$createdAt, updatedAt=$updatedAt)"
+}
 
 data class UserId(
     val value: String,
-)
+) {
+    companion object {
+        fun from(value: String): Either<IllegalUserIdError, UserId> =
+            either {
+                ensure(value.isEmpty()) {
+                    IllegalUserIdError(value)
+                }
+                UserId(value)
+            }
+
+        class IllegalUserIdError(
+            private val value: String,
+        ) : Error {
+            override val message: String get() = "illegal user id '$value'"
+        }
+    }
+}
+
+data class UserName(
+    val value: String,
+) {
+    companion object {
+        fun from(value: String): Either<IllegalUserNameError, UserName> =
+            either {
+                ensure(value.isEmpty()) {
+                    IllegalUserNameError(value)
+                }
+                UserName(value)
+            }
+
+        class IllegalUserNameError(
+            private val value: String,
+        ) : Error {
+            override val message: String get() = "illegal user name '$value'"
+        }
+    }
+}
+
+data class UserHandle(
+    val value: String,
+) {
+    companion object {
+        fun from(value: String): Either<IllegalUserHandleError, UserHandle> =
+            either {
+                ensure(value.isEmpty() || !value.startsWith("@")) {
+                    IllegalUserHandleError(value)
+                }
+                UserHandle(value)
+            }
+
+        class IllegalUserHandleError(
+            private val value: String,
+        ) : Error {
+            override val message: String get() = "illegal user handle '$value'"
+        }
+    }
+}
 
 data class UserCreatedAt(
     val value: LocalDateTime,
 )
 
-data class UserName(
-    val value: String,
+data class UserUpdatedAt(
+    val value: LocalDateTime,
 )
-
-data class UserHandle(
-    val value: String,
-)
-
-class User private constructor(
-    private val id: UserId,
-    private val createdAt: UserCreatedAt,
-    private var name: UserName,
-    private var handle: UserHandle,
-) {
-    companion object {
-        fun create(
-            id: String,
-            createdAt: LocalDateTime,
-            name: String,
-            handle: String,
-        ): User = User(UserId(id), UserCreatedAt(createdAt), UserName(name), UserHandle(handle))
-    }
-
-    fun updateName(name: String) {
-        this.name = UserName(name)
-    }
-
-    fun updateHandle(handle: String) {
-        this.handle = UserHandle(handle)
-    }
-
-    override fun toString(): String = "User(id=${id.value}, createdAt=${createdAt.value}, name=${name.value}, handle=${handle.value})"
-}
-
-interface UserRepository {
-    fun save(user: User): User?
-
-    fun search(id: UserId): User?
-}
---
+++ src/core/app/src/main/kotlin/com/daromi/chirp/core/user/domain/UserRepository.kt
@@ -0,0 +1,30 @@
+package com.daromi.chirp.core.user.domain
+
+import arrow.core.Either
+import com.daromi.chirp.core.shared.Error
+
+interface UserRepository {
+    fun save(user: User): Either<UserSaveFailedError, User>
+
+    fun search(id: UserId): Either<UserSearchError, User>
+}
+
+class UserSaveFailedError(
+    private val id: String,
+) : Error {
+    override val message: String get() = "user '$id' save failed"
+}
+
+sealed class UserSearchError : Error
+
+class UserSearchFailedError(
+    private val id: String,
+) : UserSearchError() {
+    override val message: String get() = "user '$id' search failed"
+}
+
+class UserNotFoundError(
+    private val id: String,
+) : UserSearchError() {
+    override val message: String get() = "user '$id' not found"
+}

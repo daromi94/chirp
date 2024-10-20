897f5d6f46ba601e9a1735636b57aea54d1a830c
daromi94 <davromero07@gmail.com>
2024-10-20 01:36:53 -0500

    style: :art: format post

--- src/core/app/src/main/kotlin/com/daromi/chirp/core/posts/domain/Post.kt
+++ src/core/app/src/main/kotlin/com/daromi/chirp/core/posts/domain/Post.kt
@@ -2,50 +2,59 @@ package com.daromi.chirp.core.posts.domain
 
 import com.daromi.chirp.core.users.domain.UserId
 
-class Post private constructor(
+class Post
+private constructor(
     private val id: PostId,
     private val userId: UserId,
     private var content: PostContent,
 ) {
-    companion object {
-        fun create(
-            rawId: String,
-            rawUserId: String,
-            rawContent: String,
-        ): Post? {
-            val id = PostId.from(rawId) ?: return null
-            val userId = UserId.from(rawUserId) ?: return null
-            val content = PostContent.from(rawContent) ?: return null
+  companion object {
+    fun create(
+        rawId: String,
+        rawUserId: String,
+        rawContent: String,
+    ): Post? {
+      val id = PostId.from(rawId) ?: return null
+      val userId = UserId.from(rawUserId) ?: return null
+      val content = PostContent.from(rawContent) ?: return null
 
-            return Post(id, userId, content)
-        }
+      return Post(id, userId, content)
     }
+  }
 
-    fun update(rawContent: String): Boolean {
-        this.content = PostContent.from(rawContent) ?: return false
+  fun update(rawContent: String): Boolean {
+    this.content = PostContent.from(rawContent) ?: return false
 
-        return true
-    }
+    return true
+  }
 
-    override fun toString(): String = "Post(id=${this.id}, userId=${this.userId}, content=${this.content})"
+  override fun toString(): String {
+    return "Post(${this.id.value}, ${this.userId.value}, ${this.content.value})"
+  }
 }
 
 @JvmInline
-value class PostId(
-    val value: String,
-) {
-    companion object {
-        fun from(value: String): PostId? = if (value.isBlank()) null else PostId(value)
+value class PostId(val value: String) {
+
+  companion object {
+    fun from(value: String): PostId? {
+      if (value.isBlank()) return null
+
+      return PostId(value)
     }
+  }
 }
 
 @JvmInline
-private value class PostContent(
-    val value: String,
-) {
-    companion object {
-        private const val MAX_LENGTH: Int = 280
+private value class PostContent(val value: String) {
 
-        fun from(value: String): PostContent? = if (value.isBlank() || value.length > MAX_LENGTH) null else PostContent(value)
+  companion object {
+    private const val MAX_LENGTH: UShort = 280u
+
+    fun from(value: String): PostContent? {
+      if (value.isBlank() || value.length.toUShort() > MAX_LENGTH) return null
+
+      return PostContent(value)
     }
+  }
 }

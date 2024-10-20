122fd0288acbf6c82e8db4a74496ec8d0b3704db
daromi94 <davromero07@gmail.com>
2024-10-20 01:16:16 -0500

    style: :art: format user repository

--- src/core/app/src/main/kotlin/com/daromi/chirp/core/users/domain/UserRepository.kt
+++ src/core/app/src/main/kotlin/com/daromi/chirp/core/users/domain/UserRepository.kt
@@ -1,9 +1,9 @@
 package com.daromi.chirp.core.users.domain
 
 interface UserRepository {
-    fun save(user: User)
+  fun save(user: User)
 
-    fun search(id: UserId): User?
+  fun search(id: UserId): User?
 
-    fun exists(id: UserId): Boolean
+  fun exists(id: UserId): Boolean
 }

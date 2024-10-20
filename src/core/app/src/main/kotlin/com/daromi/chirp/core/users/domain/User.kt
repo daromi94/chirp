47a9911a291ce724f3b9aefbe380102260047711
daromi94 <davromero07@gmail.com>
2024-10-20 01:15:46 -0500

    style: :art: format user

--- src/core/app/src/main/kotlin/com/daromi/chirp/core/users/domain/User.kt
+++ src/core/app/src/main/kotlin/com/daromi/chirp/core/users/domain/User.kt
@@ -1,64 +1,74 @@
 package com.daromi.chirp.core.users.domain
 
-class User private constructor(
+class User
+private constructor(
     private val id: UserId,
     private var name: UserName,
     private var handle: UserHandle,
 ) {
-    companion object {
-        fun create(
-            rawId: String,
-            rawName: String,
-            rawHandle: String,
-        ): User? {
-            val id = UserId.from(rawId) ?: return null
-            val name = UserName.from(rawName) ?: return null
-            val handle = UserHandle.from(rawHandle) ?: return null
+  companion object {
+    fun create(
+        rawId: String,
+        rawName: String,
+        rawHandle: String,
+    ): User? {
+      val id = UserId.from(rawId) ?: return null
+      val name = UserName.from(rawName) ?: return null
+      val handle = UserHandle.from(rawHandle) ?: return null
 
-            return User(id, name, handle)
-        }
+      return User(id, name, handle)
     }
+  }
 
-    fun updateName(rawName: String): Boolean {
-        this.name = UserName.from(rawName) ?: return false
+  fun rename(rawName: String): Boolean {
+    this.name = UserName.from(rawName) ?: return false
 
-        return true
-    }
+    return true
+  }
 
-    fun updateHandle(rawHandle: String): Boolean {
-        this.handle = UserHandle.from(rawHandle) ?: return false
+  fun updateHandle(rawHandle: String): Boolean {
+    this.handle = UserHandle.from(rawHandle) ?: return false
 
-        return true
-    }
+    return true
+  }
 
-    override fun toString(): String = "User(id=${this.id}, name=${this.name}, handle=${this.handle})"
+  override fun toString(): String = "User(id=${this.id}, name=${this.name}, handle=${this.handle})"
 }
 
 @JvmInline
-value class UserId(
-    val value: String,
-) {
-    companion object {
-        fun from(value: String): UserId? = if (value.isBlank()) null else UserId(value)
+value class UserId(val value: String) {
+
+  companion object {
+    fun from(value: String): UserId? {
+      if (value.isBlank()) return null
+
+      return UserId(value)
     }
+  }
 }
 
 @JvmInline
-private value class UserName(
-    val value: String,
-) {
-    companion object {
-        fun from(value: String): UserName? = if (value.isBlank()) null else UserName(value)
+private value class UserName(val value: String) {
+
+  companion object {
+    fun from(value: String): UserName? {
+      if (value.isBlank()) return null
+
+      return UserName(value)
     }
+  }
 }
 
 @JvmInline
-private value class UserHandle(
-    val value: String,
-) {
-    companion object {
-        private const val PREFIX: String = "@"
+private value class UserHandle(val value: String) {
 
-        fun from(value: String): UserHandle? = if (value.isBlank() || !value.startsWith(PREFIX)) null else UserHandle(value)
+  companion object {
+    private const val PREFIX: String = "@"
+
+    fun from(value: String): UserHandle? {
+      if (value.isBlank() || !value.startsWith(PREFIX)) return null
+
+      return UserHandle(value)
     }
+  }
 }

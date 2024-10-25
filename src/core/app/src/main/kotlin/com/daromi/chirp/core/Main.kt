3c26d28bd0b063b0a85161818d4539c2b57c6a7b
daromi94 <davromero07@gmail.com>
2024-10-24 19:59:02 -0500

    refactor: :fire: remove deprecated app module

--- src/core/app/src/main/kotlin/com/daromi/chirp/core/Main.kt
+++
@@ -1,5 +0,0 @@
-package com.daromi.chirp.core
-
-fun main() {
-  println("Hello, World!")
-}
--- src/core/app/src/main/kotlin/com/daromi/chirp/core/users/domain/UserRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.daromi.chirp.core.users.domain
-
-interface UserRepository {
-  fun save(user: User)
-
-  fun search(id: UserId): User?
-
-  fun exists(id: UserId): Boolean
-}

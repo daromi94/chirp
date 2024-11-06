b46589eacd5776969a5791f17d1d9a1d7998e000
daromi94 <davromero07@gmail.com>
2024-11-05 20:27:55 -0500

    build: :construction_worker: use snapshot version

--- src/core/build.gradle.kts
+++ src/core/build.gradle.kts
@@ -2,7 +2,7 @@ plugins { alias(libs.plugins.kotlin.jvm) apply false }
 
 group = "com.daromi.chirp.core"
 
-version = "0.1.0"
+version = "0.1.0-SNAPSHOT"
 
 subprojects {
     repositories { mavenCentral() }

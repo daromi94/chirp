b4e2322857261c9f72b971254ea94cf4492c7c82
daromi94 <davromero07@gmail.com>
2025-01-05 11:13:58 -0500

    refactor: :fire: remove superfluous spacing

--- src/core/app/build.gradle.kts
+++ src/core/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
-
     alias(libs.plugins.kotlin.spring)
     alias(libs.plugins.spring.dep)
     alias(libs.plugins.spring.boot)
--- src/core/users/build.gradle.kts
+++ src/core/users/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
-
     alias(libs.plugins.kotlin.spring)
 }
 
